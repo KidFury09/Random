e96877cb2bb9b35a220176fe9e7577004d8e6015
KidFury09 <108571822+KidFury09@users.noreply.github.com>
2022-10-21 07:45:11 +0400

    Add files via upload

---
+++ Maths.py
@@ -0,0 +1,22 @@
+from calendar import c
+
+
+def quad_form():
+    eqn = input("Enter the eqaution in <ax^2 + bx + c>: ")
+    L = eqn.split("x")
+    a = int(L[0])
+    b = int(L[1][2:])
+    c = int(L[2])
+
+    return(quad_root(a, b, c))
+
+def quad_root(a, b, c):
+    D = b**2 - 4*a*c
+    if D < 0:
+        return "Roots of this eqaution don't exist"
+    elif D == 0:
+        return (-b + D**0.5)/2*a
+    else:
+        return (-b + D**0.5)/2*a, (-b - D**0.5)/2*a
+
+print(quad_form())

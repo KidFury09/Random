be195609788bfeb8d489986c576a5556a5d4eed1
KidFury09 <108571822+KidFury09@users.noreply.github.com>
2022-10-28 20:41:38 +0400

    Update Math_V1.py

--- Math_V1.py
+++ Math_V1.py
@@ -1,9 +1,11 @@
+from math import sin
+
 def quad_form():
     eqn = input("Enter the eqaution in <ax^2 + bx + c>: ")
     L = eqn.split("x")
-    a = int(L[0])
-    b = int(L[1][2:])
-    c = int(L[2])
+    a = float(L[0])
+    b = float(L[1][2:])
+    c = float(L[2])
 
     return(quad_root(a, b, c))
 
@@ -16,4 +18,28 @@ def quad_root(a, b, c):
     else:
         return (-b + D**0.5)/2*a, (-b - D**0.5)/2*a
 
-print(quad_form())
+def vector_mag():
+    v = input("Enter the form <ai + bj + ck>: ")
+    s = ""
+    l = list(v)
+
+    for i in l:
+        if i in "+-":
+            s += " " # + i can be used for places where sing is neccessary
+        elif i.isnumeric():
+            s += i
+    
+    s = s.split()
+    l = []
+
+    for i in s:
+        l.append(float(i))
+    
+    s = 0
+    for i in l:
+        s += i ** 2
+
+    return s ** 0.5
+
+def mag_cross_prod(m1, m2, a):
+    return m1 * m2 * sin(a)
